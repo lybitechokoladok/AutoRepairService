08d75832ab2bd33c5131fa4e9856f18c9f02a480
Nikita <pashok.89iq@mail.ru>
2024-02-14 15:01:54 +0300

    add relay command

---
+++ AutoRepairService.WPF/Commands/RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AutoRepairService.WPF.Commands
+{
+    public abstract class RelayCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Predicate<object> _predicate;
+
+        protected RelayCommand(Action<object> execute, Predicate<object> predicate)
+        {
+            _execute = execute;
+            _predicate = predicate;
+        }
+
+        protected RelayCommand(Action<object> execute)
+        {
+            _execute = execute;
+            _predicate = null;
+        }
+
+        public event EventHandler? CanExecuteChanged 
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove {  CommandManager.RequerySuggested -= value;}
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _predicate == null ? true : _predicate(parameter); 
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute?.Invoke(parameter);  
+        }
+    }
+}
